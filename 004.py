279daff31c52476b72937ead5e8b80b4f2f4f32e
songye38 <ddev0308@gmail.com>
2023-07-19 19:14:01 +0900

    세부 수정함

--- 004.py
+++ 004.py
@@ -1,14 +1,33 @@
 import sys
 
-#STEP1 입력받기 
+
+###############################################
+# TODO
+#! alert comment
+# * important comment
+# ? 궁금한 것들
+###############################################
+
+
+###############################################
+#* step1 입력받고 원본 배열 만들기
+#* step2 부분합 배열 만들기
+#* step3 부분구간 합 만들기
+###############################################
+
+
+
+#* STEP1 입력받기 
 input = sys.stdin.readline
 n,m = list(map(int,input().split()))
 
 A_matrix = [] #원본 데이터를 담은 배열 
+
+#? 배열을 이렇게 담아놓는게 맞나?
 D_matrix = [[0] * n] * n  #구간합 데이터를 담은 배열 
 
 
-#입력받아 원본 배열 만들기 
+#* 입력받아 원본 배열 만들기 
 for i in range(n):
     lines = list(map(int,input().split()))
     A_matrix.append(lines)
@@ -17,9 +36,8 @@ print("원본 배열 A_matrix")
 print(A_matrix)
 
 
-#STEP2 구간 부분배열 만들기 
-#1행과 1열의 구간 부분배열 구하기
-#declare
+#* STEP2 구간 부분배열 만들기 
+#? 구간 부분 배열을 이렇게 두 부분으로 나누어서 하는게 맞나?
 column_temp = 0
 row_temp = 0
 
@@ -41,13 +59,9 @@ print("first line 합 배열")
 print(D_matrix)
 
 
-#STEP3 구간 부분합 구하기 
+#*STEP3 구간 부분합 구하기 
 #입력받아 원본 배열 만들기 
 for i in range(m):
     x1,y1,x2,y2 = list(map(int,input().split())) 
     result = D_matrix[x2][y2] - D_matrix[x1-1][y2] - D_matrix[x2][y1-1] + D_matrix[x1-1][y1-1]
     print(result)
-
-
-
-
