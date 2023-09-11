db16cb866676e681d848193d1ba94b066bb49cf5
songye38 <ddev0308@gmail.com>
2023-09-11 16:06:11 +0900

    add files

--- 39.py
+++ 39.py
@@ -1,16 +1,33 @@
 import math
 
-N  = int(iput())
-A = [0] * (N+1)
+N  = int(input())
+A = [0] * (10000001)
 
-for i in range(2,10000001):
+for i in range(2,len(A)):
     A[i] = i
 
-for i in range(2,int(math.sqrt(N))+1):
+for i in range(2,int(math.sqrt(len(A)))+1):
     if A[i]==0:
         continue
-    for j in range(i+i,N+1,i):
+    for j in range(i+i,len(A),i):
         A[j] = 0
 
-def palindron(n):
+def isPalindrome(target):
+    n_list = list(map(int, str(target)))
+    s = 0
+    e = len(n_list)-1
+    while s < e:
+        if n_list[s]!=n_list[e]:
+            return False
+        s+=1
+        e-=1
+    return True
+
+while True:
+    if A[i]!=0:
+        result = A[i]
+        if (isPalindrome(result)):
+            print(result)
+            break
+    i +=1
     
--- 40/40.py
+++ 40/40.py
@@ -1,5 +1,20 @@
-n, m = map(int,input().split())
-print(n,m)
+import math
 
-pow = 100
-print(n%pow)
+Min ,Max = map(int,input().split())
+Check = [False] * (Max - Min+1)
+
+for i in range(2,int(math.sqrt(Max))+1):
+    pow = i * i #제곱수
+    start_index = int(Min/pow)
+    if Min % pow !=0:
+        start_index +=1
+    for j in range(start_index, int(Max/pow)+1):
+        Check[int((j*pow)-Min)] = True
+
+count = 0
+
+for i in range(0,Max-Min+1):
+    if not Check[i]:
+        count +=1
+
+print(count)
