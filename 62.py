c0fe70675b763651b20589c94e100696bc3147d9
songye38 <ddev0308@gmail.com>
2023-09-23 17:58:21 +0900

    solve 62.py

---
+++ 62.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+
+distance = [[0 for i in range(N+1)] for j in range(N+1)]
+
+for i in range(N):
+    distance[i] = list(map(int,input().split()))
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if distance[i][k] ==1 and distance[k][j] == 1:
+                distance[i][j] == 1
+
+
+for i in range(N):
+    for j in range(N):
+        print(distance[i][j],end=' ')
+    print() 
+
