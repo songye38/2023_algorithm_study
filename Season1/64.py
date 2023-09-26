4c322f7bd8eac36c1fc440779087ee0e589965a2
songye38 <ddev0308@gmail.com>
2023-09-26 17:15:11 +0900

    Solve 64Q Solution

--- Season1/64.py
+++ Season1/64.py
@@ -14,13 +14,15 @@ N,M = map(int,input().split())
 pq = PriorityQueue()
 parent = [0] * (N+1)
 
+
+#union연산을 위해 대표노드값 초기화해주기 
 for i in range(N+1):
     parent[i] = i
 
 #1 2 1
 for _ in range(M):
     S,E,W = list(map(int,input().split()))
-    pq.put((W,S,E))
+    pq.put((W,S,E)) #! 우선순위 큐에서는 제일 앞에 있는 원소를 기준으로 정렬되므로 가중치 값을 제일 먼저 저장하기
 
 
 def find(a):
@@ -41,10 +43,10 @@ useEdge = 0
 result = 0
 
 while useEdge < N-1:
-    now = pq.get()
-    if find(now[1]) != find(now[2]):
-        union(now[1],now[2])
-        result += now[0]
+    w,s,e = pq.get() #! 하나의 값을 뽑은 다음 인덱싱으로 접근하지 말고 개별 값으로 접근하기 
+    if find(s) != find(e):
+        union(s,e)
+        result += w
         useEdge+=1
 
 print(result)
