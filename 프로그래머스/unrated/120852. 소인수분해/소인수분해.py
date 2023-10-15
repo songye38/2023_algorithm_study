8ef6d672b2d2f6121498d2c1be633525d750d89b
songye <ddev0308@gmail.com>
2023-10-16 01:59:09 +0900

    [level unrated] Title: 소인수분해, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = []
+    i = 2
+    while i <= n:
+        if n % i == 0:
+            answer.append(i)
+            n = n //i
+        else:
+            i +=1     
+    return sorted(list(set(answer)))
