7d863bf631c95f3c86cf16e99a978d21ceb927b7
songye38 <ddev0308@gmail.com>
2023-10-17 09:57:00 +0900

    Refactoring 포켓몬

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,12 +1,2 @@
-import itertools
 def solution(nums):
-    if len(nums)//2 == len(set(nums)):
-        return len(set(nums))
-    elif len(nums)//2 < len(set(nums)):
-        return len(nums)//2
-    else:
-        return len(set(nums))
-    # answer_list = [0] * (len(nums)+1)
-    # answer = set(itertools.combinations(nums,len(nums)//2))
-    # #return max([len(list(set(i))) for i in answer])
-    # return [i for i in answer]
+    return min(len(nums)/2,len(set(nums)))
